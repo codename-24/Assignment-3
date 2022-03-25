44f0fa3cb7c2a6f879d300c54ba601f8bc27e6d7
codename-24 <vijay2495@gmail.com>
2022-03-25 12:13:52 +0530

    getelementcommit

--- dom.js
+++ dom.js
@@ -1,1 +1,9 @@
 console.log("hello");
+console.log(document.getElementById('header-title'));
+var header = document.getElementById('header-title');
+header.style.borderBottom = 'solid 3px #000';
+var item = document.getElementsByClassName('title');
+console.log(item);
+var tag = document.getElementsByTagName('h2');
+console.log(tag);
+tag.style.textContent = 'yellow';
